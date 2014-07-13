53d14e0199ce2ba1bbcf91a33987279701d2e4f1
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2014-07-13 16:34:19 +0200

    added cross validation

--- anova_svm_classifier.py
+++ anova_svm_classifier.py
@@ -1,7 +1,6 @@
 from datetime import date
 
 __author__ = 'siavash'
-print(__doc__)
 
 
 import numpy as np
@@ -9,10 +8,12 @@ import pandas as pd
 from sklearn import svm
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.pipeline import Pipeline
+from sklearn import cross_validation
 
 X = np.loadtxt('train.csv', delimiter=',')
 y = np.loadtxt('trainLabels.csv', delimiter=',')
 test = np.loadtxt('test.csv', delimiter=',')
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.1,random_state=0)
 
 # ANOVA SVM-C
 # 1) anova filter, take 3 best ranked features
@@ -21,11 +22,17 @@ anova_filter = SelectKBest(f_regression, k=3)
 clf = svm.SVC(kernel='linear')
 
 anova_svm = Pipeline([('anova', anova_filter), ('svm', clf)])
-anova_svm.fit(X, y)
-prediction = anova_svm.predict(test)
+anova_svm.fit(X_train, y_train)
+print anova_svm.score(X_test, y_test)
 
-index = np.r_[1:9001]
-columns = ['Solution']
-df = pd.DataFrame(prediction,index=index, columns=columns)
-df.index.name = 'Id'
-df.to_csv('result.csv')
+def create_prediction_for_kaggle():
+    anova_filter = SelectKBest(f_regression, k=3)
+    clf = svm.SVC(kernel='linear')
+    anova_svm = Pipeline([('anova', anova_filter), ('svm', clf)])
+    anova_svm.fit(X, y)
+    prediction = anova_svm.predict(test)
+    index = np.r_[1:9001]
+    columns = ['Solution']
+    df = pd.DataFrame(prediction,index=index, columns=columns)
+    df.index.name = 'Id'
+    df.to_csv('result.csv')
--- gaussian_naive_bayes.py
+++ gaussian_naive_bayes.py
@@ -2,18 +2,23 @@ __author__ = 'siavash'
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
 import pandas as pd
+from sklearn import cross_validation
 
 X = np.loadtxt('train.csv', delimiter=',')
 y = np.loadtxt('trainLabels.csv', delimiter=',')
 test = np.loadtxt('test.csv', delimiter=',')
 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.4,random_state=0)
 gnb = GaussianNB()
-y_pred = gnb.fit(X, y).predict(X)
-test_pred = gnb.fit(X, y).predict(test)
+clf = gnb.fit(X_train, y_train)
+print clf.score(X_test, y_test)
 
-index = np.r_[1:9001]
-columns = ['Solution']
-df = pd.DataFrame(test_pred,index=index, columns=columns)
-df.index.name = 'Id'
-df.to_csv('result.csv')
-print("Number of mislabeled points : %d" % (y != y_pred).sum())
+
+def create_prediction_for_kaggle():
+    test_pred = gnb.fit(X, y).predict(test)
+    index = np.r_[1:9001]
+    columns = ['Solution']
+    df = pd.DataFrame(test_pred,index=index, columns=columns)
+    df.index.name = 'Id'
+    df.to_csv('result.csv')
+    print("Number of mislabeled points : %d" % (y != y_pred).sum())
